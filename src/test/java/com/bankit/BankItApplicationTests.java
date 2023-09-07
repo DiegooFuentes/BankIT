fb41e0bd9a541eb5e5d4a88e3b96ef078d0cf250
Diego Fuentes <112411727+DiegooFuentes@users.noreply.github.com>
2023-09-07 12:43:19 -0300

    First commit

---
+++ src/test/java/com/bankit/BankItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
