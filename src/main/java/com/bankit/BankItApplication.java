3d8d39ecbcbb911a4a508b6095b142d66f05b8a8
Diego Fuentes <112411727+DiegooFuentes@users.noreply.github.com>
2023-09-08 22:20:44 -0300

    feat: new data

--- src/main/java/com/bankit/BankItApplication.java
+++ src/main/java/com/bankit/BankItApplication.java
@@ -1,7 +1,12 @@
 package com.bankit;
 
+import com.bankit.web.models.Client;
+import com.bankit.web.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class BankItApplication {
@@ -10,4 +15,12 @@ public class BankItApplication {
         SpringApplication.run(BankItApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner initData(ClientRepository clientRepository) {
+        return (args) -> {
+            // save a couple of customers
+            clientRepository.save(new Client("Camila", "Balmaceda", "camilabal1@gmail.com", "hoLa32-"));
+
+        };
+    }
 }
