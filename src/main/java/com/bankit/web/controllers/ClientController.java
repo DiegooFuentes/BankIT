07b0666ebb3557757ade54974bcdee6962b33e90
Diego Fuentes <112411727+DiegooFuentes@users.noreply.github.com>
2023-10-23 14:42:22 -0300

    feat: API Rest with endpoints to access client data

--- src/main/java/com/bankit/web/controllers/ClientController.java
+++ src/main/java/com/bankit/web/controllers/ClientController.java
@@ -1,4 +1,34 @@
 package com.bankit.web.controllers;
 
+import com.bankit.web.dtos.ClientDTO;
+import com.bankit.web.services.ClientServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class ClientController {
+
+    private final ClientServiceImpl clientService;
+
+    @Autowired
+    public ClientController(ClientServiceImpl clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/clients")
+    public List<ClientDTO> getClients() {
+        return clientService.getClients();
+
+    }
+
+    @GetMapping("/clients/{id}")
+    public ClientDTO getClient(@PathVariable Long id) {
+        return clientService.getClient(id);
+    }
 }
