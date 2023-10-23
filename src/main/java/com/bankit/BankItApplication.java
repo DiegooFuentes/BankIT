cd8383fa517c43eaf05848702c874a1c8c6e4df4
Diego Fuentes <112411727+DiegooFuentes@users.noreply.github.com>
2023-10-23 14:39:49 -0300

    chore: New test data

--- src/main/java/com/bankit/BankItApplication.java
+++ src/main/java/com/bankit/BankItApplication.java
@@ -1,11 +1,16 @@
 package com.bankit;
 
+import com.bankit.web.models.Account;
 import com.bankit.web.models.Client;
 import com.bankit.web.repositories.ClientRepository;
+import com.bankit.web.repositories.AccountRepository;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 
 @SpringBootApplication
@@ -16,10 +21,12 @@ public class BankItApplication {
     }
 
     @Bean
-    public CommandLineRunner initData(ClientRepository clientRepository) {
+    public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository) {
         return (args) -> {
             // save a couple of customers
-            clientRepository.save(new Client("Camila", "Balmaceda", "camilabal1@gmail.com", "hoLa32-"));
+            Client client1 = clientRepository.save(new Client("Camila", "Balmaceda", "camilabal1@gmail.com", "hoLa32-"));
+            accountRepository.save(new Account("VIN001", LocalDateTime.now(),127000.0,client1));
+            clientRepository.save(new Client("Fabian", "Jackson", "fabiqueza@gmail.com", "wo1lDs12"));
 
         };
     }
