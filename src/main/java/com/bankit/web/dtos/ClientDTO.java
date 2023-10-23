dedf27e81becc0127ed8252dcc465d781daf108d
Diego Fuentes <112411727+DiegooFuentes@users.noreply.github.com>
2023-10-23 14:41:49 -0300

    feat: Add DTO

--- src/main/java/com/bankit/web/dtos/ClientDTO.java
+++ src/main/java/com/bankit/web/dtos/ClientDTO.java
@@ -1,4 +1,35 @@
 package com.bankit.web.dtos;
 
+import com.bankit.web.models.Client;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
+
+@Getter
+@Setter
 public class ClientDTO {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Set<AccountDTO> accounts;
+
+
+    public ClientDTO(Client client) {
+
+        this.id = client.getId();
+        this.firstName = client.getFirstName();
+        this.lastName = client.getLastName();
+        this.email = client.getEmail();
+        this.accounts = client.getAccounts().stream().map(AccountDTO::new).collect(toSet());
+
+    }
 }
