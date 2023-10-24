2b4b1109349ff7361d04ec8c12918550834973ba
Diego Fuentes <112411727+DiegooFuentes@users.noreply.github.com>
2023-10-24 14:55:45 -0300

    test: test data

--- src/main/java/com/bankit/BankItApplication.java
+++ src/main/java/com/bankit/BankItApplication.java
@@ -2,9 +2,12 @@ package com.bankit;
 
 import com.bankit.web.models.Account;
 import com.bankit.web.models.Client;
+import com.bankit.web.models.Transaction;
+import com.bankit.web.models.TransactionType;
 import com.bankit.web.repositories.ClientRepository;
 import com.bankit.web.repositories.AccountRepository;
 
+import com.bankit.web.repositories.TransactionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,14 +24,18 @@ public class BankItApplication {
     }
 
     @Bean
-    public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository) {
+    public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository, TransactionRepository transactionRepository) {
         return (args) -> {
             // save a couple of customers
             Client client1 = clientRepository.save(new Client("Camila", "Balmaceda", "camilabal1@gmail.com", "hoLa32-"));
-            accountRepository.save(new Account("VIN001", LocalDateTime.now(),127000.0,client1));
+            Account account1= accountRepository.save(new Account("VIN001", LocalDateTime.now(),127000.0,client1));
             accountRepository.save(new Account("VIN002", LocalDateTime.now(),99000.0,client1));
 
             clientRepository.save(new Client("Fabian", "Jackson", "fabiqueza@gmail.com", "wo1lDs12"));
+
+            transactionRepository.save(new Transaction(TransactionType.DEBIT,2000.0,"pepe",LocalDateTime.now(),account1));
+            transactionRepository.save(new Transaction(TransactionType.CREDIT,9900.0,"jaja",LocalDateTime.now(),account1));
+
 
         };
     }
