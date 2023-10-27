db296b127c21c5b6ca74025d2b6c62a0b181876f
Diego Fuentes <112411727+DiegooFuentes@users.noreply.github.com>
2023-10-27 14:00:45 -0300

    feat: add method to find card by type of card and client

--- src/main/java/com/bankit/web/repositories/CardRepository.java
+++ src/main/java/com/bankit/web/repositories/CardRepository.java
@@ -1,9 +1,15 @@
 package com.bankit.web.repositories;
 
 import com.bankit.web.models.Card;
+import com.bankit.web.models.CardType;
+import com.bankit.web.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    List<Card> findCardByTypeAndClient (CardType cardType, Client client);
 }
